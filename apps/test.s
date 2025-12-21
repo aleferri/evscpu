2e66fd55a23931e43cc29eac694fbfae73a74dcb
Alessio Ferri <alessio.ferri.3012@gmail.com>
2025-12-21 23:48:55 +0100

    fix docs part1 and correct mapping opcodes to ustate

--- apps/test.s
+++ apps/test.s
@@ -32,13 +32,12 @@ __boot:     LIT     6
             SUB     _arg0
             JZE     .ok
             JMP     __init.hard_err
-.ok:
-            LIT     __init.print
+.ok:        LIT     __init.print
             STA     _ra
             LIT     0
             ADD     _ioptr
             STA     _arg1
-            JMP     __split_bin
+            JMP     __init
             
 .include "lib.s"
 
@@ -50,38 +49,60 @@ __init:     LIT     1
             JZE     .hard_err           ; one or both of STA/ADD not working even in the base case
             LIT     4
             STA     __shadow
-            LIT     3
-            INC     __shadow
+            LIT     5
+            STA     __shadow
             LIT     5
             SUB     __shadow
             JZE     .selftest
-            LIT     0x12
+            LIT     0x1                 ; nothing work
             JMP     .hard_err
             
-.selftest:	LIT     3					; require working lit/sta/add/jze/jmp, test the other instructions    
+.selftest:	LIT     0                   ; require working lit/sta/add/jze/jmp, test the other instructions    
+            STA     __shadow
+            LIT     10
+            LDA     _shadow
+            JZE     .oklda
+.oklda:     LIT     3					
 			STA     __shadow
 			LIT     15
 			ADD     __shadow
 			LIT     3
 			SUB     __shadow
 			JZE     .oksub
-			LIT     0x10		; Math error
+			LIT     0x2		            ; Math error
 			JMP     .hard_err
 .oksub:		LIT     4
             STA     _ix0
             LIT     0
             AND     _ix0
-            JZE     .print
-            LIT     0x11
+            JZE     .okand
+            LIT     0x3                 ; And error
             JMP     .hard_err
 .okand:     LIT     0xFF
-;.oknor:
-;.okinc:
+            STA     __shadow
+            LIT     112
+            NOR     __shadow
+            JZE     .oknor
+            LIT     0x4                 ; Nor error
+            JMP     .hard_err
+.oknor:     LIT     1
+            JNZ     .okjnz
+            LIT     0x5                 ; Comparison error
+            JMP     .hard_err
+.okjnz      LIT     0
+            STA     __shadow
+            INC     __shadow
+            LDA     __shadow
+            JNZ     .okinc
+            LIT     0x6                 ; Inc error
+            JMP     .hard_err
+.okinc:
+;.okjpi:
+;.okshr:
 ;.okldi:
 ;.oksti:
 
-.print:		LIT     0                   ; _ix0 = 0xFFF0
-            ADD     _ioptr              
+.print:		LDA     _ioptr              ; _ix0 = 0xFFF0          
 
 .dg0:       STA     _arg1
             LIT     8
